1b413909b6421e80dfe5ac813e78c93b58832d03
AlanPieczonka <alanpieczonka@gmail.com>
2018-03-12 18:27:49 +0100

    Add rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -20,14 +20,13 @@ module BackendVotingApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => :any
+      end
+    end
     config.api_only = true
   end
 end
